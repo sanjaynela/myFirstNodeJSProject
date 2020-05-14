1f1653a1818cae95425a24b0e010ae3d2a4a3ff4
Sanjay <ballacksanjay@hotmail.com>
2020-05-14 09:48:41 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var calc = require('./calc.js');
+
+result1 = calc.add(5,3);
+console.log("Result1 : " + result1);
+
+result2 = calc.minus(5,3);
+console.log("Result2 : " + result2);
+
+result3 = calc.prime(2);
+console.log("Result3 of 2 : " + result3);
+
+result3 = calc.prime(3);
+console.log("Result3 of 3 : " + result3);
+
+result3 = calc.prime(4);
+console.log("Result3 of 4 : " + result3);
---
+++ calc.js
@@ -0,0 +1,22 @@
+
+exports.add = function add(a,b){
+    return a + b;
+}
+
+exports.minus = function minus(a,b){
+    return a - b;
+}
+
+function prime(a){
+    if(a==0 || a==1){
+        return false;
+    }
+    var isPrime = true;
+    for(i=2;i<a;i++){
+        if(a % i == 0){
+            isPrime = false;
+        }
+    }
+    return isPrime;
+}
+exports.prime = prime;
