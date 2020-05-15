92cf1a16d29a2fa294ec1c4544baaeebc632bb74
Sanjay <ballacksanjay@hotmail.com>
2020-05-15 00:36:09 -0700

    Add File Systems Methods

---
+++ read.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+
+fs.readFile('calc.js','utf8',function(err,data){
+    console.log(data);
+});
+
+fs.writeFile('writeFile.txt','This is a test','utf8',function(err){
+    if(err){
+        return console.error(err);
+    }
+});
